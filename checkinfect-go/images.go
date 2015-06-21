1a6e4696b46b40a2f6eb8feb11d19128060ad909
Peter Rossbach <peterrossbach@me.com>
2015-06-21 07:33:46 -0700

    Add docker unix sock support and fix list all containers
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- checkinfect-go/images.go
+++ checkinfect-go/images.go
@@ -2,13 +2,15 @@ package main
 
 import (
     "fmt"
+    "strings"
     "os"
     "github.com/fsouza/go-dockerclient"
+    "encoding/json"
 )
 
 type CheckInfect struct {
-	images []docker.APIImages
-  containers []docker.APIContainers
+  images     []docker.APIImages     `json:"images,omitempty"`
+  containers []docker.APIContainers `json:"containers,omitempty"`
 }
 
 func check (client *docker.Client, infect_id string ) *CheckInfect {
@@ -22,7 +24,7 @@ func check (client *docker.Client, infect_id string ) *CheckInfect {
       }
     }
   }
-  containers, _ := client.ListContainers(docker.ListContainersOptions{All: false})
+  containers, _ := client.ListContainers(docker.ListContainersOptions{All: true})
   for _, container := range containers {
     himgs, _ := client.ImageHistory(container.Image)
     for _, himg := range himgs {
@@ -36,13 +38,31 @@ func check (client *docker.Client, infect_id string ) *CheckInfect {
 }
 
 func main() {
+    var client *docker.Client
+
     endpoint := os.Getenv("DOCKER_HOST")
-    path := os.Getenv("DOCKER_CERT_PATH")
-    ca := fmt.Sprintf("%s/ca.pem", path)
-    cert := fmt.Sprintf("%s/cert.pem", path)
-    key := fmt.Sprintf("%s/key.pem", path)
-    client, _ := docker.NewTLSClient(endpoint, cert, key, ca)
+    if ( endpoint != "") {
+      if (strings.HasPrefix(endpoint, "tcp://")) {
+        path := os.Getenv("DOCKER_CERT_PATH")
+        if( path != "") {
+          ca := fmt.Sprintf("%s/ca.pem", path)
+          cert := fmt.Sprintf("%s/cert.pem", path)
+          key := fmt.Sprintf("%s/key.pem", path)
+          client, _ = docker.NewTLSClient(endpoint, cert, key, ca)
+        } else {
+          client, _ = docker.NewClient(endpoint)
+        }
+      } else {
+        client, _ = docker.NewClient(endpoint)
+      }
+    } else {
+      endpoint := "unix:///var/run/docker.sock"
+      client, _ = docker.NewClient(endpoint)
+    }
 
     infect_id := os.Getenv("INFECT_ID")
-    fmt.Println(check(client,infect_id))
+    infect := check(client,infect_id)
+    fmt.Println(infect)
+    data, _ := json.Marshal(infect)
+    fmt.Println(string(data))
 }
