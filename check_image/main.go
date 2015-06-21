3bc65c9d0a26747f8339304291cce649272fc50c
Daniel Sachse <mail@wombatsoftware.de>
2015-06-20 20:57:09 -0700

    Added vendor script to check for vulnerabilities

---
+++ check_image/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	endpoint := os.Getenv("DOCKER_HOST")
+	path := os.Getenv("DOCKER_CERT_PATH")
+	ca := fmt.Sprintf("%s/ca.pem", path)
+	cert := fmt.Sprintf("%s/cert.pem", path)
+	key := fmt.Sprintf("%s/key.pem", path)
+	client, _ := docker.NewTLSClient(endpoint, cert, key, ca)
+
+	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+
+	for _, img := range imgs {
+		for i := range args {
+
+			config := &docker.Config{Image: img.ID, Cmd: []string{"/script/shell.sh"}}
+
+			// Create the container from an id, specify the command, mount CVE volume
+			container, err := client.CreateContainer(docker.CreateContainerOptions{Config: config})
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			// Get the working directory
+			pwd, err := os.Getwd()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			binds := []string{pwd + "/cve/" + args[i] + "/:/script"}
+
+			hostConfig := &docker.HostConfig{Binds: binds}
+			err = client.StartContainer(container.ID, hostConfig)
+
+			code, err := client.WaitContainer(container.ID)
+
+			if code > 0 {
+				fmt.Println("Image " + img.ID + " is vulnerable to " + args[i] + "!")
+			}
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
+}
